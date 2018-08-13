cda12aead24c3b21b80152d8a5d5220496297364
Nitin Chaurasia <gs.nitin@gmail.com>
2018-08-12 19:16:49 -0700

    Minor Changes to the Project

--- basics-components/src/app/success-alert/success-alert.component.ts
+++ basics-components/src/app/success-alert/success-alert.component.ts
@@ -6,7 +6,8 @@ import { Component } from "@angular/core";
     //The html tag by which this component will be used later
     // should me a unique selector, so that the html name space is not used accidently
     selector : 'app-success-alert',
-    templateUrl: './success-alert.component.html'
+    templateUrl: './success-alert.component.html',
+    styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent{
     
--- basics-components/src/app/warning-alert/warning-alert.component.ts
+++ basics-components/src/app/warning-alert/warning-alert.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
+  // BEWARE OF THE Template. Template is used to define in-line html template
+  // Use Template URL for pointing to an external File
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css']
 })
